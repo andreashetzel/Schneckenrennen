48b6cb354b689392ce2459a7b7a9030b9a662e6e
Hetzel.Andreas <Hetzel.Andreas@DESKTOP-K96C4OE.2631.skool.net>
2018-10-18 09:10:48 +0200

    complete Rennschnecke

--- src/Schneckenrennen.java
+++ src/Schneckenrennen.java
@@ -6,6 +6,11 @@ public class Schneckenrennen {
 		Rennschnecke schnecki1 = new Rennschnecke("Hasi", "Glitscho", 3);
 		Rennschnecke schnecki2 = new Rennschnecke("Baerchen", "Rasanto", 5);
 
+		
+		schnecki1.krieche();
+		System.out.println(schnecki1);
+		schnecki1.krieche();
+		System.out.println(schnecki1);
 	}
 
 }
