0a04dd3c6949d9f184cb5f5f584ea4ab50a457fe
Hetzel.Andreas <Hetzel.Andreas@DESKTOP-K96C4OE.2631.skool.net>
2018-10-18 08:36:51 +0200

    Rennschencke

--- src/Schneckenrennen.java
+++ src/Schneckenrennen.java
@@ -2,7 +2,9 @@
 public class Schneckenrennen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Rennschnecke schnecki1 = new Rennschnecke("Hasi", "Glitscho", 3);
+		Rennschnecke schnecki2 = new Rennschnecke("Baerchen", "Rasanto", 5);
 
 	}
 
